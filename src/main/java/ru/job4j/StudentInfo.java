0289cde417b139c09d9d88276e179b4d301c79f7
warlocknt <info@tagilnet.ru>
2021-12-09 13:31:26 +0500

    added Multiple

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ramil Mukhetdinov");
+        System.out.println("26.12.1978");
+    }
+}
