a5598e2e0e5cb64bde5ea29eff4943e6014e752a
warlocknt <info@tagilnet.ru>
2021-12-09 23:50:53 +0500

    Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,18 +3,29 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70; /* формула перевода рублей в евро. */
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
+        return value / 60; /* формула перевода рублей в доллары. */
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(420);
-        float dollars = Converter.rubleToDollar(420);
-        System.out.println("140 rubles are " + euro + " euro and " + dollars + " dollars.");
+
+        float in = 420;
+        float expected = 6;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("420 rubles are 6 euro. Test result : " + passed);
+
+        expected = 7;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("420 rubles are 7 dollars. Test result : " + passed);
+
+        //float rubles = 420;
+        //float euro = Converter.rubleToEuro(420);
+        //float dollars = Converter.rubleToDollar(420);
+        //System.out.println(rubles + " rubles are " + euro + " euro and " + dollars + " dollars.");
     }
 }
