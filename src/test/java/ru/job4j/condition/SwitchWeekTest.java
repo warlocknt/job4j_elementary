94d925f0843f2eae1ea76cf75810ef9ab61cf921
warlocknt <info@tagilnet.ru>
2021-12-11 00:53:04 +0500

    Switch [#334070]

---
+++ src/test/java/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class SwitchWeekTest {
+
+    @Test
+    public void whenDay1() {
+        int day = 1;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Понедельник";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay2() {
+        int day = 2;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Вторник";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay3() {
+        int day = 3;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Среда";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay4() {
+        int day = 4;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Четверг";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay5() {
+        int day = 5;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Пятница";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay6() {
+        int day = 6;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Суббота";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay7() {
+        int day = 7;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Воскресенье";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDay8() {
+        int day = 8;
+        String result = SwitchWeek.nameOfDay(day);
+        String expected = "Ошибка";
+        Assert.assertEquals(expected, result);
+    }
+}
