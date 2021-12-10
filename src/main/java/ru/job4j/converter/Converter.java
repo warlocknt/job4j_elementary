b4176efdc1506216094fe9dfe6105969175885f4
warlocknt <info@tagilnet.ru>
2021-12-10 12:03:36 +0500

    all comments removed

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,11 +3,11 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 70; /* формула перевода рублей в евро. */
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        return value / 60; /* формула перевода рублей в доллары. */
+        return value / 60;
     }
 
     public static void main(String[] args) {
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
