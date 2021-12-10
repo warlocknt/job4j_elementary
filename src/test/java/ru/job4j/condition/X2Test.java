dff3328c1e8d46003c2b2f00beb6c36f5b733fe7
warlocknt <info@tagilnet.ru>
2021-12-10 18:13:56 +0500

    temp commit w/o comments

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -55,7 +55,7 @@ public class X2Test {
         int b = 1;
         int c = 1;
         int x = 0;
-        int expected = 2;
+        int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
