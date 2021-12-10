c142e6d75e40569101576b5a6b374987fb8065bb
warlocknt <info@tagilnet.ru>
2021-12-10 23:21:21 +0500

    temp commit

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,7 +3,7 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 70;1
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
