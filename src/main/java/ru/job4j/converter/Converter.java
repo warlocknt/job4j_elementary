d06db562de412991b613e19d1439002cb67123fd
warlocknt <info@tagilnet.ru>
2021-12-10 23:20:31 +0500

    Что такое тестирование [#159431] #2

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,7 +3,7 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        return value / 70;
+        return value / 70;1
     }
 
     public static float rubleToDollar(float value) {
