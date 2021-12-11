43d3d21af421e65152c12a45769427340f24e512
warlocknt <info@tagilnet.ru>
2021-12-12 01:32:27 +0500

    сделал методы приватными и сделал тесты только на публичный метод

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class ChessBoard {
 
-    public static boolean validateCoordinatesAndWay(int x1, int y1, int x2, int y2) {
-        return validateCoordinate(x1) && validateCoordinate(y1) && validateCoordinate(x2) && validateCoordinate(y2) && Math.abs(x1 - x2) == Math.abs(y1 - y2);
+    private static boolean validateCoordinate(int coordinate) {
+        return (coordinate >= 0) && (coordinate <= 7);
     }
 
-    public static boolean validateCoordinate(int coordinate) {
-        return (coordinate >= 0) && (coordinate <= 7);
+    private static boolean validateCoordinatesAndWay(int x1, int y1, int x2, int y2) {
+        return validateCoordinate(x1) && validateCoordinate(y1) && validateCoordinate(x2) && validateCoordinate(y2) && Math.abs(x1 - x2) == Math.abs(y1 - y2);
     }
 
     public static int way(int x1, int y1, int x2, int y2) {
