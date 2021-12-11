450b85d23b1544c9ec8c00690f875ebd6262d5cf
warlocknt <info@tagilnet.ru>
2021-12-11 13:29:57 +0500

    избавился от else

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -3,15 +3,21 @@ package ru.job4j.condition;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
+        boolean conditionFound = false;
+        rsl = "Исходное число не делится на 3 и не является четным.";
+
         if (LogicNot.isEven(number) && isDividedBy3(number)) {
             rsl = "Исходное число делится на 6.";
-        } else if (isDividedBy3(number) && LogicNot.notEven(number)) {
-            rsl = "Исходное число делится на 3, но не является четным.";
-        } else if (notDividedBy3(number) && LogicNot.isEven(number)) {
-            rsl = "Исходное число не делится на 3, но является четным.";
-        } else {
-            rsl = "Исходное число не делится на 3 и не является четным.";
+            conditionFound = true;
         }
+        if (isDividedBy3(number) && LogicNot.notEven(number) && !conditionFound) {
+            rsl = "Исходное число делится на 3, но не является четным.";
+            conditionFound = true;
+        }
+        if (notDividedBy3(number) && LogicNot.isEven(number) && !conditionFound) {
+            rsl = "Исходное число не делится на 3, но является четным.";
+        }
+
         return rsl;
     }
 
